2c1c58d81d13b66d7469cda4214ca2bea6b7387a
Felipe Ribeiro <piporibeiro@gmail.com>
2023-05-16 22:44:59 -0300

    feat: start home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
+import { User } from 'lucide-react'
+
 export default function Home() {
   return (
-    <div className={'bg-transparent text-2xl font-bold uppercase'}>
-      Hello World!
-    </div>
+    <main className={'grid min-h-screen grid-cols-2'}>
+      {/* Left */}
+      <div className="relative  flex flex-col justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes" />
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta </span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+      </div>
+
+      {/* Right */}
+      <div
+        className={
+          'flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16'
+        }
+      >
+        <div className={'flex flex-1 items-center justify-center'}>
+          <p className={'w-[360px] text-center leading-relaxed'}>
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
