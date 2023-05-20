2eafb687277cbc79b1b5215503d312f688f56b7c
Felipe Ribeiro <piporibeiro@gmail.com>
2023-05-20 14:24:36 -0300

    refactor: remove padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
 
           <div
             className={
-              'flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16'
+              'flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover'
             }
           >
             {children}
